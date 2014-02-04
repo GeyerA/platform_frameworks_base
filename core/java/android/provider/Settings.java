1b593d5fdd00b7f63420bcf98cb4ef9e5b224260
GeyerA <calo.geyer@gmail.com>
2014-02-04 21:33:23 +0100

    Added flip to mute - Credits to Mahdi

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2679,6 +2679,14 @@ public final class Settings {
         public static final String QUIET_HOURS_DIM = "quiet_hours_dim";
 
         /**
+         * Whether flip action during incomming call should mute or dismiss
+         * the call (mute = 0, dismiss = 1, nothing = 2 (default))
+         *
+         * @hide
+         */
+        public static final String CALL_FLIP_ACTION_KEY = "call_flip_action_key";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
