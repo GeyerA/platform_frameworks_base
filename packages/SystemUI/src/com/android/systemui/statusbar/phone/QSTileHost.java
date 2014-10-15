fc783696d814995190e2003a214cec8930f6aa8c
kaiyiz <kaiyiz@codeaurora.org>
2014-10-15 10:20:02 +0800

    SystemUI: Add sound item in quick settings panel
    Add sound item in quick settings panel.
    
    CRs-Fixed: 715258
    
    Change-Id: I090929a3622101edc5413afe4f7c2d1863139d2c
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
    
    Conflicts:
    	packages/SystemUI/res/values/config.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -38,6 +38,7 @@ import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
+import com.android.systemui.qs.tiles.NotificationsTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.settings.CurrentUserTracker;
@@ -52,6 +53,7 @@ import com.android.systemui.statusbar.policy.HotspotController;
 import com.android.systemui.statusbar.policy.SecurityController;
 import com.android.systemui.statusbar.policy.UserSwitcherController;
 import com.android.systemui.statusbar.policy.ZenModeController;
+import com.android.systemui.volume.VolumeComponent;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -78,6 +80,7 @@ public class QSTileHost implements QSTile.Host {
     private final CastController mCast;
     private final Looper mLooper;
     private final CurrentUserTracker mUserTracker;
+    private final VolumeComponent mVolume;
     private final FlashlightController mFlashlight;
     private final UserSwitcherController mUserSwitcherController;
     private final KeyguardMonitor mKeyguard;
@@ -88,7 +91,7 @@ public class QSTileHost implements QSTile.Host {
     public QSTileHost(Context context, PhoneStatusBar statusBar,
             BluetoothController bluetooth, LocationController location,
             RotationLockController rotation, NetworkController network,
-            ZenModeController zen, HotspotController hotspot,
+            ZenModeController zen, VolumeComponent volume, HotspotController hotspot,
             CastController cast, FlashlightController flashlight,
             UserSwitcherController userSwitcher, KeyguardMonitor keyguard,
             SecurityController security) {
@@ -99,6 +102,7 @@ public class QSTileHost implements QSTile.Host {
         mRotation = rotation;
         mNetwork = network;
         mZen = zen;
+        mVolume = volume;
         mHotspot = hotspot;
         mCast = cast;
         mFlashlight = flashlight;
@@ -188,6 +192,11 @@ public class QSTileHost implements QSTile.Host {
     }
 
     @Override
+    public VolumeComponent getVolumeComponent() {
+        return mVolume;
+    }
+
+    @Override
     public HotspotController getHotspotController() {
         return mHotspot;
     }
@@ -244,6 +253,7 @@ public class QSTileHost implements QSTile.Host {
         else if (tileSpec.equals("location")) return new LocationTile(this);
         else if (tileSpec.equals("cast")) return new CastTile(this);
         else if (tileSpec.equals("hotspot")) return new HotspotTile(this);
+        else if (tileSpec.equals("notifications")) return new NotificationsTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
