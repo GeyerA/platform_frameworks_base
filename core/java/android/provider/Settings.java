2027f912f332712ac7e0d6fe3aa5346c8031f878
tsubus <tsubus@ilwt.org>
2014-12-05 20:47:49 +0100

    Base: Implement ad blocker (1/3)
    *Hosts File Manager
    *Picked from Dirty Unicorns
    *This uses two prebuilt hosts files and switches between them by symlinking
    
    Change-Id: I23a1d9f1dcca623a556defb5eaae94f4e9e3b21d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2874,6 +2874,13 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_WEATHER = "status_bar_show_weather";
 
         /**
+         * Ad blocker
+	 * Disable ads (HFM)
+	 * @hide
+	 */
+	public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
