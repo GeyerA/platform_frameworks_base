4cab2caaaf4290226eb416a54ace219c6ab1d15d
Austin T. Conn <atc3030dev@gmail.com>
2014-12-17 13:28:00 -0500

    volume rocker music controls
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2796,6 +2796,12 @@ public final class Settings {
         public static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
 
         /**
+         * Whether or not volume button music controls should be enabled to seek media tracks
+         * @hide
+         */
+        public static final String VOLUME_ROCKER_MUSIC_CONTROLS = "volume_rocker_music_controls";
+
+        /**
          * Show or hide clock
          * 0 - hide
          * 1 - show (default)
