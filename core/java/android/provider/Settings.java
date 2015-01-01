1e7f0ba2dee90ede6e8f6048b62b222e7a3d8c04
Evisceration <eviscerationls@gmail.com>
2015-01-01 14:29:50 +0000

    [1/2] Quick settings: Add Haptic Feedback to tiles
    Change-Id: I792a00a2689a572110963db00362f8e9d77b316e
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/qs/QSPanel.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2819,6 +2819,12 @@ public final class Settings {
          */
         public static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
 
+        /** Whether to vibrate when quick settings tile is pressed.
+         *
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_TILES_VIBRATE = "quick_settings_vibrate";
+       
         /**
          * Show or hide clock
          * 0 - hide
