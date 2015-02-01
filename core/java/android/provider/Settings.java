1aa381cbedd75a867abd9ec4c87c2a27a93bda05
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-01 06:41:03 +0000

    QS: Option to show four tiles per row (1/2)
    This also reduces the size of the ripple when using 4 tiles per row
    
    Change-Id: Id61d6f2b113feabda46f464d0eda51c811226dd8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5174,6 +5174,12 @@ public final class Settings {
         public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "lockscreen_quick_unlock_control";
 
         /**
+         * Whether to show four tiles per row.
+         * @hide
+         */
+        public static final String QS_USE_FOUR_COLUMNS = "qs_use_four_columns";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
