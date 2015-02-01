089d5e8ad32587af0d6d6f919faadf61cccb3fbc
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-01 06:41:03 +0000

    QS: Option to show four tiles per row (1/2)
    This also reduces the size of the ripple when using 4 tiles per row
    
    Change-Id: Id61d6f2b113feabda46f464d0eda51c811226dd8
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/qs/QSTileView.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
    	packages/SystemUI/src/com/android/systemui/qs/QSTileView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6183,6 +6183,11 @@ public final class Settings {
                 "lockscreen_hide_qs_tiles_with_sensitive_data";
 
         /**
+         * Whether to show four tiles per row.
+         * @hide
+         */
+        public static final String QS_USE_FOUR_COLUMNS = "qs_use_four_columns";
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
